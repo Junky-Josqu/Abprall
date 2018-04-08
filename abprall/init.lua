12f78cfafad8b68cd7c19009f117e5da86a51fd3
John <jgonline@live.de>
2018-04-08 17:58:38 +0200

    integrate GScreen

--- abprall/init.lua
+++ abprall/init.lua
@@ -1,8 +1,20 @@
 --init.lua
 local wf = require('abprall.lib.windfield.windfield')
---require('abprall.lib.catui.catui')
+require('abprall.lib.catui.init')
 local CScreen = require ("abprall.lib.CScreen.cscreen")
 
 local ap = {}
+function ap.load()
+  CScreen.init(800, 600, true)
+end
 
+function ap.draw()
+	CScreen.apply()
+
+  CScreen.cease()
+end
+
+function love.resize(width, height)
+	CScreen.update(width, height)
+end
 return ap
