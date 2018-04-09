c3a2ad043cef9f253c613755bee133ee6138e2d3
John <jgonline@live.de>
2018-04-09 22:00:32 +0200

    build ap.update() and require some libs

--- abprall/init.lua
+++ abprall/init.lua
@@ -1,11 +1,17 @@
 --init.lua
 local wf = require('abprall.lib.windfield.windfield')
 require('abprall.lib.catui.init')
+local gui = require('abprall.lib.Gspot')
 local CScreen = require ("abprall.lib.CScreen.cscreen")
+local SaveData = require('abprall.lib.SaveData')
 
 local ap = {}
 function ap.load()
   CScreen.init(800, 600, true)
+end
+
+function ap.update()
+  
 end
 
 function ap.draw()
@@ -13,6 +19,7 @@ function ap.draw()
 
   CScreen.cease()
 end
+
 
 function love.resize(width, height)
 	CScreen.update(width, height)
--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 -- main.lua for testing only
 local ap = require('abprall')
+
 function love.load()
   ap.load()
+end
+
+function love.update()
+  ap.update()
 end
 
 function love.draw()
